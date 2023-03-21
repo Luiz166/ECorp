085ac859cb7dec25b6f936443206b7e5baaff5a6
Luiz Otavio <otavio.tata.morais@gmail.com>
2023-03-21 14:51:19 -0300

    preventDefault

--- js/login.js
+++ js/login.js
@@ -3,7 +3,8 @@ const passwordInput = document.querySelector("#inputPassword");
 
 
 //eyeslash show password or disable password
-floatingSpan.addEventListener("click", () =>{
+floatingSpan.addEventListener("click", (e) =>{
+    e.preventDefault();
     if(floatingSpan.querySelector("i").classList.contains("fa-eye-slash")){
         passwordInput.type="password";
         floatingSpan.querySelector("i").className="fa-solid fa-eye";
